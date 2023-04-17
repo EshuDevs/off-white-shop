364dfd94bfc1da0bbe9be8ebd535bfffb37f3eff
Eshuyaleo <115549948+Eshuyaleo@users.noreply.github.com>
2023-04-17 19:41:02 +0200

    added cards with props

--- off-white-store/src/components/ProductList.tsx
+++ off-white-store/src/components/ProductList.tsx
@@ -3,22 +3,30 @@ interface Product {
     name: string;
     price: string;
   }
+  interface ProductWithImageProps extends Product {
+    image: string;
+  }
   
   interface ProductListProps extends React.PropsWithChildren<{}> {
-    products: Product[];
+    products: ProductWithImageProps[];
   }
 
 const ProductList = (props: ProductListProps) =>{
-    const products = props.products;
+const products = props.products;
 
 
 return(
     <>
-        <div className="flex justify-evenly">
+        <div className="font-kanit pb-4 text-5xl font-bold mt-14 mx-44">Shoes</div>
+        <div className="flex flex-wrap gap-12 justify-center my-10">
             {products.map((products) =>(
-                <div key={products.id}>
-                    <h2>{products.name}</h2>
-                    <p>{products.price}</p>
+                <div key={products.id} className="p-6 mx-7 border-2 bg-gray-100 border-gray-600 border-solid w-1/6">
+                    <div className="flex bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-400 via-gray-900 to-black align-center justify-center">
+                        <img src={products.image}/>
+                    </div>
+                    <h2 className="mt-12">{products.name}</h2>
+                    <p className="mb-4">{products.price}</p>
+                    <button className="my-4 rounded-lg p-2 font-inter bg-black font-semibold text-white border border-black">Add to Cart</button>
                 </div>
             ))}
         </div>
--- off-white-store/src/components/Products.tsx
+++ off-white-store/src/components/Products.tsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import ProductList from "./ProductList"
+import unc from "../assets/unc.png"
+import whiteSail from "../assets/white-sail.png"
+import jordanFive from "../assets/jordan-five.png"
+import airForce from "../assets/air-force.png"
+import serena from "../assets/serena.png"
+import vaporMax from "../assets/vapormax.png"
+import dunk from "../assets/dunk.png"
+import presto from "../assets/presto.png"
 
 const Products = () => {
     const [products] = useState([
-        { id: 1, name: "OFF-WHITE x NIKE Jordan 4 White Sail", price: "299.99$" },
-        { id: 2, name: "Product",price: "299.99$"},
+        { id: 1, image: whiteSail ,name: "OFF-WHITE x NIKE Jordan 4 White Sail", price: "299.99$" },
+        { id: 2, image: unc, name: "OFF-WHITE x NIKE Jordan 1 High UNC" ,price: "349.99$"},
+        { id: 1, image: jordanFive, name: "OFF-WHITE x NIKE Jordan 4 White Sail", price: "299.99$" },
+        { id: 1, image: airForce, name: "OFF-WHITE x NIKE Jordan 4 White Sail", price: "299.99$" },
+        { id: 1, image: serena, name: "OFF-WHITE x NIKE Jordan 4 White Sail", price: "299.99$" },
+        { id: 1, image: vaporMax, name: "OFF-WHITE x NIKE Jordan 4 White Sail", price: "299.99$" },
+        { id: 1, image: dunk, name: "OFF-WHITE x NIKE Jordan 4 White Sail", price: "299.99$" },
+        { id: 1, image: presto, name: "OFF-WHITE x NIKE Jordan 4 White Sail", price: "299.99$" }
 
     ])
 
